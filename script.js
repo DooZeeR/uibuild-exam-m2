f11dd512ce5e85a67475b308d23a6a84da8578b6
DooZeeR <zsolt.ordas@gmail.com>
2021-12-13 15:22:09 +0100

    Asszem kész

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const formHTML = `
+    <form action="">
+        <div><h1>Visszajelzés</h1></div>
+        <div><input type="text" name="name" id="name" placeholder="Tárgy"></div>
+        <div class="textareadiv"><textarea id="textarea" name="textarea" rows="2" cols="55" placeholder="Megjegyzés"></textarea></div>
+        <div class="check"><input type="checkbox" name="data" id="data">
+        <label for="data">Elolvastam és elfogadom az <a href="">Adatkezelési tájékoztató</a></label></div>
+        <div><button type="submit">MENTÉS</button></div>
+    </form>
+`;
+
+
+
+
+
+function loadEvent() {
+    const rootElement = document.getElementById('root');
+
+    rootElement.insertAdjacentHTML('beforeend', formHTML);
+
+
+
+
+
+}
+  
+window.addEventListener("load", loadEvent);
+
+
+
+
+
